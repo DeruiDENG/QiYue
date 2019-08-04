4fbf0085e89c55ef8f276873f033f16fa4d42c1a
Derui DENG <moonface.ddr@gmail.com>
2019-08-04 12:06:17 +0800

    Add redux, react-router

--- webpack.config.js
+++ webpack.config.js
@@ -9,17 +9,14 @@ module.exports = {
     path: __dirname + '/dist',
   },
 
-  // Enable sourcemaps for debugging webpack's output.
   devtool: 'source-map',
 
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: ['.ts', '.tsx', '.js', '.json']
   },
 
   module: {
     rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {
         test: /\.scss$/,
         use: [
