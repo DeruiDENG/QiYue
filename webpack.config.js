a0e370b9e430b2452fb14f46d458792fee108710
Derui DENG <moonface.ddr@gmail.com>
2019-08-08 22:37:31 +0800

    Add docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,80 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "bundle.[hash].js",
-    path: __dirname + "/dist"
-  },
+module.exports = function(env) {
+  const isProduction = env === "prod";
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: "./src/index.tsx",
+    output: {
+      filename: "bundle.[hash].js",
+      path: isProduction ? `${__dirname}/docs` : `${__dirname}/dist`
+    },
 
-  devtool: "source-map",
+    devtool: "source-map",
 
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader" // translates CSS into CommonJS
-          },
-          {
-            loader: "less-loader", // compiles Less to CSS
-            options: {
-              javascriptEnabled: true
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+          ]
+        },
+        {
+          test: /\.less$/,
+          use: [
+            {
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader" // translates CSS into CommonJS
+            },
+            {
+              loader: "less-loader", // compiles Less to CSS
+              options: {
+                javascriptEnabled: true
+              }
             }
+          ]
+        },
+        {
+          test: /\.(svg|png|jpe?g|gif)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {}
+            }
+          ]
+        },
+        {
+          test: /\.tsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      },
-      {
-        test: /\.(svg|png|jpe?g|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {}
-          }
-        ]
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader"
         }
-      }
-    ]
-  },
+      ]
+    },
 
-  devServer: {
-    contentBase: [path.join(__dirname, "dist")],
-    compress: true,
-    port: 9000
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "public/index.html"
-    })
-  ]
+    devServer: {
+      contentBase: [path.join(__dirname, "dist")],
+      compress: true,
+      port: 9000
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "public/index.html"
+      })
+    ]
+  };
 };
